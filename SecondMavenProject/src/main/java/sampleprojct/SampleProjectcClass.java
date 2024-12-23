85d1e7b00985c674569799220d71af1df0fa01df
msman <msman@192.168.1.6>
2024-12-23 23:42:19 +0530

    THIS IS THE FIRST COMMIT IN 2ND MAVEN PROJECT

---
+++ SecondMavenProject/src/main/java/sampleprojct/SampleProjectcClass.java
@@ -0,0 +1,11 @@
+package sampleprojct;
+
+public class SampleProjectcClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is From Second Maven Project");
+
+	}
+
+}
